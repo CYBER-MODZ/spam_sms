3087e2de1cbb5f4f7c1e01db90914caaf0ec8e80
CYBER-MODZ <92875143+CYBER-MODZ@users.noreply.github.com>
2021-10-20 23:32:44 +0700

    Spamv1.py

--- spamv1.py
+++ spamv1.py
@@ -149,7 +149,7 @@ def countdownTimer(start_minute, start_second):
 
 if __name__ == '__main__':
     for i in range(int(jumlah)):
-        darko=requests.post('https://api.duniagames.co.id/api/transaction/v1/top-up/transaction/req-otp/',('https://www.tokocash.com/oauth/otp)',headers=mr_f,json=dark).text
+        darko=requests.post('https://api.duniagames.co.id/api/transaction/v1/top-up/transaction/req-otp/',headers=mr_f,json=dark).text
         print (f'  \033[31m➤ \033[1;37mTerkirim                            ')
         countdownTimer(00, 60)
         subs_mr_dark += 1
